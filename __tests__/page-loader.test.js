9ceaa9b90c3c9d563ae4af168525edc4393e7825
NRog <norogozin@gmail.com>
2023-01-21 23:15:40 +0400

    tests updated

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -3,6 +3,7 @@ import { readFile, mkdtemp } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import os from 'node:os';
+import { expect } from '@jest/globals';
 import pageLoad from '../src/page-loader.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,27 +11,29 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (fileName) => join(__dirname, '..', '__fixtures__', fileName);
 
+const pageUrl = 'https://ru.hexlet.io';
+
 const readFixtureFileContent = (fileName, encoding = 'utf-8') => readFile(getFixturePath(fileName), encoding);
 
 nock.disableNetConnect();
-const scope = nock('https://ru.hexlet.io').persist();
+const scope = nock(pageUrl).persist();
 
 const serverErrors = [
   {
     code: 404,
-    desciption: 'Page not found',
+    description: 'Page not found',
   },
   {
     code: 401,
-    desciption: 'Unauthorized',
+    description: 'Unauthorized',
   },
   {
     code: 505,
-    desciption: 'Internal server error',
+    description: 'Internal server error',
   },
   {
     code: 503,
-    desciption: 'Service unavailable',
+    description: 'Service unavailable',
   },
 ];
 
@@ -56,25 +59,13 @@ const assets = [
     encoding: 'utf-8',
   },
 ];
-beforeAll(async () => {
-  outputDir = await mkdtemp(`${os.tmpdir()}/page-loader-test`);
-});
 
 describe('Page loader', () => {
-  describe('error codes tests', () => {
-    test.each(serverErrors)('testing code %s', async (error) => {
-      scope.get(`/test${error.code}`)
-        .reply(error.code, error.desciption);
-      scope.get(`/test${error.code}`)
-        .reply(error.code, error.desciption);
-      await expect(pageLoad(`https://ru.hexlet.io/test${error.code}`)).rejects.toThrow(`${error.code}`);
-    });
-  });
-
-  it('main test', async () => {
-    const expectedHTML = await readFixtureFileContent('./expected/ru-hexlet-io-courses.html');
+  let expectedHTML;
+  const expectedFiles = {};
+  beforeAll(async () => {
+    expectedHTML = await readFixtureFileContent('./expected/ru-hexlet-io-courses.html');
     const htmlToDownload = await readFixtureFileContent('./ru-hexlet-io-courses.html');
-    const expectedFiles = {};
     await Promise.all(assets.map(async (asset) => {
       expectedFiles[asset.urlPath] = await readFixtureFileContent(
         asset.fixturePath,
@@ -83,24 +74,67 @@ describe('Page loader', () => {
       scope.get(asset.urlPath).reply(200, expectedFiles[asset.urlPath]);
     }));
     scope.get('/courses').reply(200, htmlToDownload);
-    await pageLoad('https://ru.hexlet.io/courses', outputDir);
-    const actualHTML = await readFile(`${outputDir}/ru-hexlet-io-courses.html`, 'utf-8');
-    const actualFiles = {};
-    await Promise.all(assets.map(async (asset) => {
-      actualFiles[asset.urlPath] = await readFile(`${outputDir}/${asset.outputPath}`, asset.encoding);
-    }));
-    const expectedFileNames = Object.keys(expectedFiles);
-    await Promise.all(expectedFileNames.map(async (fileName) => {
-      await expect(actualFiles[fileName]).toEqual(expectedFiles[fileName]);
-    }));
-    await expect(actualHTML).toEqual(expectedHTML);
+  });
+  describe('negative tests', () => {
+    beforeAll(async () => {
+      outputDir = await mkdtemp(`${os.tmpdir()}/page-loader-test`);
+    });
+    test.each(serverErrors)('testing code %s', async (error) => {
+      scope.get(`/test${error.code}`)
+        .reply(error.code, error.desciption);
+      await expect(pageLoad(`${pageUrl}/test${error.code}`)).rejects.toThrow(`${error.code}`);
+    });
+    it('file system error', async () => {
+      const rootDirPath = '/sys';
+      await expect(
+        pageLoad(pageUrl.toString(), rootDirPath),
+      ).rejects.toThrow();
+    });
+    it('Directory not exists', async () => {
+      const rootDirPath = '/notExisting';
+      await expect(
+        pageLoad(`${pageUrl}/courses`, rootDirPath),
+      ).rejects.toThrow('EACCES: permission denied');
+    });
+    it('Directory not available', async () => {
+      const rootDirPath = '/sys';
+      await expect(
+        pageLoad(`${pageUrl}/courses`, rootDirPath),
+      ).rejects.toThrow('EACCES: permission denied');
+    });
   });
 
-  it('load page: file system errors', async () => {
-    const pageUrl = 'http://localhost/';
-    const rootDirPath = '/sys';
-    await expect(
-      pageLoad(pageUrl.toString(), rootDirPath),
-    ).rejects.toThrow();
+  describe('positive tests', () => {
+    beforeEach(async () => {
+      outputDir = await mkdtemp(`${os.tmpdir()}/page-loader-test`);
+    });
+    it('successful page download', async () => {
+      await pageLoad(`${pageUrl}/courses`, outputDir);
+      const actualHTML = await readFile(`${outputDir}/ru-hexlet-io-courses.html`, 'utf-8');
+      const actualFiles = {};
+      await Promise.all(assets.map(async (asset) => {
+        actualFiles[asset.urlPath] = await readFile(`${outputDir}/${asset.outputPath}`, asset.encoding);
+      }));
+      const expectedFileNames = Object.keys(expectedFiles);
+      await Promise.all(expectedFileNames.map(async (fileName) => {
+        await expect(actualFiles[fileName]).toEqual(expectedFiles[fileName]);
+      }));
+      await expect(actualHTML).toEqual(expectedHTML);
+    });
+
+    it('successful page download (with output option)', async () => {
+      // cd outputDir
+      process.chdir(outputDir);
+      await pageLoad(`${pageUrl}/courses`);
+      const actualHTML = await readFile(`${outputDir}/ru-hexlet-io-courses.html`, 'utf-8');
+      const actualFiles = {};
+      await Promise.all(assets.map(async (asset) => {
+        actualFiles[asset.urlPath] = await readFile(`${outputDir}/${asset.outputPath}`, asset.encoding);
+      }));
+      const expectedFileNames = Object.keys(expectedFiles);
+      // eslint-disable-next-line max-len
+      expectedFileNames.forEach((fileName) => expect(actualFiles[fileName]).toEqual(expectedFiles[fileName]));
+      await expect(actualHTML).toEqual(expectedHTML);
+    });
   });
 });
